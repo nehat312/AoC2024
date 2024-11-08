3f2a3f00301c2ae18582d6b1aa8ec8e432eef5dd
Landcruiser87 <andyheroy@gmail.com>
2024-11-07 19:02:31 -0600

    updates

--- 2024/utils/day_template.py
+++ 2024/utils/day_template.py
@@ -1,17 +1,16 @@
 import os
 import sys
-root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(root_folder)
 from utils.time_run import log_time
 from utils.loc import recurse_dir
 from utils import support
 from pathlib import Path, PurePath
 from rich.console import Console
 from datetime import datetime
+root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_folder)
 
-
-def data_load(DAY:str, filen:str)->list:
-    with open(f'{DAY}{filen}.txt', 'r') as f:
+def data_load(fp:str)->list:
+    with open(f'{fp}.txt', 'r') as f:
         data = f.read().splitlines()
         arr = [x.strip() if x != "" else "" for x in data]
     return arr
@@ -20,23 +19,24 @@ def data_load(DAY:str, filen:str)->list:
 def part_A(fp:str):
     data = data_load(fp)
 
-
 @log_time
 def part_B(fp:str):
     data = data_load(fp)
 
 def main():
-    DAY = './day1/'
+    #Set day variable
+    global DAY
+    DAY = 1
     current_date = datetime.now().strftime("%m-%d-%YT%H:%M:%S")
-    log_path = PurePath(Path.cwd(), Path(f"./{DAY}/{current_date}.log"))
-    fp = PurePath(Path.cwd(), Path(f"./data/{DAY}/day.txt"))
+    log_path = PurePath(Path.cwd(), Path(f"./2024/day{DAY}/_{current_date}.log"))
+    fp = PurePath(Path.cwd(), Path(f"./2024/day{DAY}.txt"))
     #Logger setup
     global logger, console
     console = Console()
-    logger = support.get_logger(log_path, console)		
-    logger.info(f"Part A solution: \n{part_A(DAY)}\n")
-    logger.info(f"Part B solution: \n{part_B(DAY)}\n")
-    logger.info(f"Lines of code \n{recurse_dir(DAY)}")
+    logger = support.get_logger(log_path, console)
+    logger.info(f"Part A solution: \n{part_A(fp)}\n")
+    logger.info(f"Part B solution: \n{part_B(fp)}\n")
+    logger.info(f"Lines of code \n{recurse_dir(f'./day{DAY}')}")
 
 if __name__ == "__main__":
     main()
