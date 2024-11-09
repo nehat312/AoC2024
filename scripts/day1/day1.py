2a68ef61c1c1c1d14b0ff734ed4fce500b217bf3
Landcruiser87 <andyheroy@gmail.com>
2024-11-09 17:08:18 -0600

    updates

--- scripts/day1/day1.py
+++ scripts/day1/day1.py
@@ -3,11 +3,9 @@ import sys
 #Add the dir above day run as path for easy import
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_folder)
-from utils.support import log_time, recurse_dir, logger #console
+from utils.support import log_time, recurse_dir, logger, console
 from utils import support
 from datetime import datetime
-
-
 
 @log_time
 def process_input(textdata:str, split:bool=True)->list:
@@ -20,7 +18,7 @@ def process_input(textdata:str, split:bool=True)->list:
 
 @log_time
 def part_A(DAY:int, YEAR:int):
-    logger.info(support.pull_puzzle(DAY, YEAR, 1, logger))
+    console.log(f"\n{support.pull_puzzle(DAY, YEAR, 1, logger)}")
     input = support.pull_inputdata(DAY, YEAR, logger)
     data = process_input(input) #Include False to not split
     logger.info("Test data processed/loaded")
@@ -29,7 +27,7 @@ def part_A(DAY:int, YEAR:int):
 @log_time
 def part_B(DAY:int, YEAR:int, test_data):
     #Solve part B
-    logger.info(support.pull_puzzle(DAY, YEAR, 2, logger))
+    console.log(f"\n{support.pull_puzzle(DAY, YEAR, 2, logger)}")
     data = test_data
     logger.info("test data processed/loaded")
 
@@ -38,14 +36,15 @@ def main():
     DAY = 1
     YEAR = 2023 #datetime.now().year
     
-    
     #Solve part A
     resultA, test_data = part_A(DAY, YEAR)
     logger.info(f"Part A solution: \n{resultA}\n")
-
+    
+    #Solve part B
     resultB = part_B(DAY, YEAR, test_data)
     logger.info(f"Part B solution: \n{resultB}\n")
-
+    
+    #Recurse lines of code
     logger.info(f"Lines of code \n{recurse_dir(f'./scripts/day{DAY}/')}")
 
 if __name__ == "__main__":
