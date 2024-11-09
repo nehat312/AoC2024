1377f8c7a90ccd666f1c846c838feccb1fabde5a
Landcruiser87 <andyheroy@gmail.com>
2024-11-09 17:38:59 -0600

    update template

--- scripts/utils/day_template.py
+++ scripts/utils/day_template.py
@@ -3,54 +3,53 @@ import sys
 #Add the dir above day run as path for easy import
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_folder)
-
-from utils.support import log_time
-from utils.loc import recurse_dir
+from utils.support import log_time, recurse_dir, logger, console
 from utils import support
-from pathlib import Path, PurePath
-from rich.console import Console
 from datetime import datetime
 
-
 @log_time
-def data_load(fp:str)->list:
-    #TODO.  Make sure you have the right file path
-    with open(fp, 'r') as f:
-        data = f.read().splitlines()
+def process_input(textdata:str, split:bool=True)->list:
+    if split:
+        data = textdata.splitlines()
         arr = [x.strip() if x != "" else "" for x in data]
+    else:
+        arr = [x.strip() if x != "" else "" for x in textdata]
     return arr
 
 @log_time
-def part_A(fp:str):
-    data = data_load(fp)
-    logger.info("Test data loaded")
+def part_A(DAY:int, YEAR:int):
+    puzzletext, data = support.pull_puzzle(DAY, YEAR, 1, logger)
+    console.log(f"\n{puzzletext}")
+    data = process_input(data)
+    # input = support.pull_inputdata(DAY, YEAR, logger)
+    # data = process_input(input) #Include False to not split
+    return "duh", data
 
 @log_time
-def part_B(fp:str):
-    data = data_load(fp)
-    logger.info("Test data loaded")
+def part_B(DAY:int, YEAR:int, test_data):
+    #Solve part B
+    console.log(f"\n{support.pull_puzzle(DAY, YEAR, 2, logger)}")
+    data = test_data
+    return None
 
 def main():
     #Set day variable
-    global DAY
-    DAY = 1
-    current_date = datetime.now().strftime("%m-%d-%YT%H:%M:%S")
-    root_folder = Path.cwd()
-    log_path = PurePath(root_folder, Path(f"./scripts/day{DAY}/_{current_date}.log"))
-    fp = PurePath(root_folder, Path(f"./scripts/day{DAY}/test_data.txt"))
-
-    #Logger setup
-    global logger, console
-    console = Console()
-    logger = support.get_logger(log_path, console)
-
-    logger.info(f"Part A solution: \n{part_A(fp)}\n")
-    logger.info(f"Part B solution: \n{part_B(fp)}\n")
-    logger.info(f"Lines of code \n{recurse_dir(f'./scripts/day{DAY}')}")
+    DAY = 1 #datetime.now().day
+    YEAR = 2023 #datetime.now().year
+    
+    #Solve part A
+    resultA, test_data = part_A(DAY, YEAR)
+    logger.info(f"Part A solution: \n{resultA}\n")
+    
+    #Solve part B
+    resultB = part_B(DAY, YEAR, test_data)
+    logger.info(f"Part B solution: \n{resultB}\n")
+    
+    #Recurse lines of code
+    logger.info(f"Lines of code \n{recurse_dir(f'./scripts/day{DAY}/')}")
 
 if __name__ == "__main__":
     main()
-
 
 ########################################################
 #Notes
