8409f3a30e0acc330bafcff72d389be56c9e44aa
Andy Heroy <aheroy@mail.smu.edu>
2024-11-10 17:42:14 -0600

    Update day1.py

--- scripts/day1/day1.py
+++ scripts/day1/day1.py
@@ -46,6 +46,7 @@ def part_B():
     console.log(f"\n{puzzletext}")
     #process/print the testdata
     testdata = support.process_input(testdata)
+    #move logger to process input
     [logger.warning(f"{td}") for td in testdata]
     #TODO - Write solution functions for testcase
     testcase = "duh"
