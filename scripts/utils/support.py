6bd07eb9b5e37dd6e7343cb62681a8e60c8557f6
Andy Heroy <aheroy@mail.smu.edu>
2024-11-10 19:00:04 -0600

    Update support.py

--- scripts/utils/support.py
+++ scripts/utils/support.py
@@ -20,6 +20,9 @@ with open("./secret/cookie.txt", "r") as f:
 cache = percache.Cache(".cache", livesync=True)
 cache.expire = timedelta(hours=1)
 
+#TODO - Create func that can add rows and / or update a markdown table.   or store it in a dataclass.  I'd like it to be able to add new days and update it as I complete sections.  
+
+
 ################################# Timing Funcs ##############################
 def log_time(fn):
     """
