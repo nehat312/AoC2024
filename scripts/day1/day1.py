9c965d8e06478f9fd37986896d49558ad6420034
Landcruiser87 <andyheroy@gmail.com>
2024-12-01 17:20:20 -0600

    day 1 complete!

--- scripts/day1/day1.py
+++ scripts/day1/day1.py
@@ -6,13 +6,36 @@ sys.path.append(root_folder)
 from utils.support import log_time, logger, console, _877_cache_now
 from utils import support
 from datetime import datetime
+from collections import Counter
 
 #Set day/year global variables
 DAY:int = datetime.now().day
 YEAR:int = datetime.now().year
 
-# def problemsolver(arr:list, part:int):
-#     pass
+def problemsolver(arr:list, part:int):
+    #Transform input strings to two lists
+    left, right, diffs = [], [], []
+    for row in arr:
+        l, r = row.split()
+        left.append(int(l))
+        right.append(int(r))
+        
+    #Sort the lists
+    left = sorted(left)
+    right = sorted(right)
+
+    if part == 1:
+        #Find their differences
+        for l, r in zip(left, right):
+            diffs.append(abs(l - r))
+    
+    if part == 2:
+        for val in left:
+            if val in right:
+                res = Counter(right)
+                diffs.append(val * res[val])
+
+    return sum(diffs)
 
 @log_time
 def part_A():
@@ -25,11 +48,11 @@ def part_A():
     # console.log(f"{tellstory}")
     # [logger.info(row) for row in testdata]
     #Solve puzzle w/testcase
-    testcase = "" #problemsolver(testdata, 1)
+    testcase = problemsolver(testdata, 1)
     #Assert testcase
-    assert testcase == "", "Test case A failed"
+    assert testcase == 11, "Test case A failed"
     #Solve puzzle with full dataset
-    answerA = "" #problemsolver(data, 1)
+    answerA = problemsolver(data, 1)
     return answerA
 
 @log_time
@@ -42,11 +65,11 @@ def part_B():
     # console.log(f"{tellstory}")
     # [logger.info(row) for row in testdata]
     #Solve puzzle w/testcase
-    testcase = "" #problemsolver(testdata, 2)
+    testcase = problemsolver(testdata, 2)
     #Assert testcase
-    assert testcase == "", "Test case B failed"
+    assert testcase == 31, "Test case B failed"
     #Solve puzzle with full dataset
-    answerB = "" #problemsolver(data, 2)
+    answerB = problemsolver(data, 2)
     return answerB
 
 def main():
@@ -59,9 +82,9 @@ def main():
     # support.submit_answer(DAY, YEAR, 1, resultA)
 
     #Solve part B
-    # resultB = part_B()
-    # logger.info(f"part B solution: \n{resultB}\n")
-    # support.submit_answer(DAY, YEAR, 2, resultB)
+    resultB = part_B()
+    logger.info(f"part B solution: \n{resultB}\n")
+    support.submit_answer(DAY, YEAR, 2, resultB)
 
     #Recurse lines of code
     LOC = support.recurse_dir(f'./scripts/day{DAY}/')
@@ -76,3 +99,17 @@ if __name__ == "__main__":
 ########################################################
 #Notes
 #Part A Notes
+#The theme this year is searching for the Chief Historian!  Everyday will be a
+#new location where we will try to find their location before Xmas!  They were sent out
+#on expedition to scout historical sites and they've gone missing!  
+#
+#Starting with an empty list, The search will start at the chief historians office. He's not there
+#But they did find lists of where they were going to go for the year.   As the historians 
+#split into two groups, each groups tries to reconcile a list of location id's of where
+#the chief historian might have gone.  
+#
+#Initially the two lists are very disimilar.  
+# 
+# 1. We'll first sort each list from smallest to largest
+# 2. for each pair, what is their difference. 
+# 3. Add up the list of differences.  Easy peasy
