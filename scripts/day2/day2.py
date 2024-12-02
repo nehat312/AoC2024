080dcbf72c78e609295060fb0eb6cb8b1096cca7
Landcruiser87 <andyheroy@gmail.com>
2024-12-02 07:17:05 -0600

    part 1 complete

--- scripts/day2/day2.py
+++ scripts/day2/day2.py
@@ -6,13 +6,32 @@ sys.path.append(root_folder)
 from utils.support import log_time, _877_cache_now, logger, console
 from utils import support
 from datetime import datetime
+import numpy as np
 
 #Set day/year global variables
 DAY:int = datetime.now().day
 YEAR:int = datetime.now().year
 
-# def problemsolver(arr:list, part:int):
-#     pass
+def check_requirements(row:list) -> bool:
+    diffs = np.diff(row)
+    test1 = np.all(diffs > 0) | np.all(diffs < 0)
+    difftest = np.where((abs(diffs) < 1) | abs(diffs) > 3)[0]
+    test2 = difftest.size == 0
+    if test1 & test2:
+        return True
+    else:
+        return False
+    
+def problemsolver(arr:list, part:int) -> int:
+    safezone = 0
+    for row in arr:
+        row = [int(x) for x in row.split()]
+        if check_requirements(row):
+            safezone += 1
+    if part == 1:
+        return safezone
+    if part == 2:
+        pass
 
 @log_time
 def part_A():
@@ -25,11 +44,11 @@ def part_A():
     # console.log(f"{tellstory}")
     # [logger.info(row) for row in testdata]
     #Solve puzzle w/testcase
-    testcase = "" #problemsolver(testdata, 1)
+    testcase = problemsolver(testdata, 1)
     #Assert testcase
-    assert testcase == "", "Test case A failed"
+    assert testcase == 2, "Test case A failed"
     #Solve puzzle with full dataset
-    answerA = "" #problemsolver(data, 1)
+    answerA = problemsolver(data, 1)
     return answerA
 
 @log_time
@@ -56,11 +75,11 @@ def main():
     #Solve part A
     resultA = part_A()
     logger.info(f"part A solution: \n{resultA}\n")
-    # support.submit_answer(DAY, YEAR, 1, resultA)
+    support.submit_answer(DAY, YEAR, 1, resultA)
 
     #Solve part B
-    resultB = part_B()
-    logger.info(f"part B solution: \n{resultB}\n")
+    # resultB = part_B()
+    # logger.info(f"part B solution: \n{resultB}\n")
     # support.submit_answer(DAY, YEAR, 2, resultB)
 
     #Recurse lines of code
@@ -76,3 +95,14 @@ if __name__ == "__main__":
 ########################################################
 #Notes
 #Part A Notes
+#Now we are looking in a nuclear power plant (Ironic that nuclear power is back
+#in the story, just as people are starting to accept it for our power hungry LLM
+#training!) We can't find the historian here, but the elves there would like us
+#to take alook at some odd reports.  Our puzzle input, are one of many reports.
+#One report is one line, and each line is separated a list of number reffered
+#too as levels.
+#Our goal:
+#Deem which reports are safe.  
+# **If all levels are increasing or decreasing** Check Monotocity
+# **Any two levels differ by at least one and at most 3**
+# We need to report how many of these levels are safe.  
